2e1254b99dd1ceb7f38db548c4babf11e58408c1
Santiago Mejia <santiago.mejia@jalasoft.com>
2022-02-06 13:57:07 -0500

    a

--- FULLFIBERSA/src/server.js
+++ FULLFIBERSA/src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-/* app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/ng-blog'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
-}); */
+});
 
-app.listen(process.env.PORT || 4200, function(){
+app.listen('0.0.0.0', process.env.PORT || 3000, function(){
     console.log("Node app is running at localhost:" + app.get('port'));
   });
