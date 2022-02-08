eb436410e456735cdf940a5ed9060227bfa3d0b1
Santiago Mejia <santiago.mejia@jalasoft.com>
2022-02-07 19:11:24 -0500

    Pruning app

--- FULLFIBERSA/src/app/Componentes/dashboard/dashboard-routing.module.ts
+++ FULLFIBERSA/src/app/Componentes/dashboard/dashboard-routing.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogsDeCambioComponent } from '../logs-de-cambio/logs-de-cambio.component';
-import { AsignarCampanhasComponent } from './asignar-campanhas/asignar-campanhas.component';
 import { DashboardComponent } from './dashboard.component';
-import { GenerarReporteComponent } from './generar-reporte/generar-reporte.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { ModificandoClienteCampanhaComponent } from './modificando-cliente-campanha/modificando-cliente-campanha.component';
-import { ModificarClienteCampanhaComponent } from './modificar-cliente-campanha/modificar-cliente-campanha.component';
 import { RegistrarCampanhasComponent } from './registrar-campanhas/registrar-campanhas.component';
-import { RegistrarProductosComponent } from './registrar-productos/registrar-productos.component';
 
 
 const routes: Routes = [
   {path: '', component: DashboardComponent, children : [
-      {path: 'inicio', component: InicioComponent },
-      {path: 'registrarProductos', component: RegistrarProductosComponent},
       {path: 'ovas', component: RegistrarCampanhasComponent },
-      {path: 'modificarCampañas', component: ModificarClienteCampanhaComponent },
-      {path: 'asignarCampañas', component: AsignarCampanhasComponent },
-      {path: 'reporte', component: GenerarReporteComponent },
-      {path: 'reporteLogs', component: LogsDeCambioComponent },
-      {path: 'modificandoCliente', component: ModificandoClienteCampanhaComponent}
-
+      {path: 'reporteLogs', component: LogsDeCambioComponent }
   ]}
 ];
 
--- FULLFIBERSA/src/app/Componentes/dashboard/dashboard.module.ts
+++ FULLFIBERSA/src/app/Componentes/dashboard/dashboard.module.ts
@@ -5,25 +5,13 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CompartidosModule } from '../compartidos/compartidos.module';
 import { DashboardComponent } from './dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { GenerarReporteComponent } from './generar-reporte/generar-reporte.component';
-import { InicioComponent } from './inicio/inicio.component';
 import { RegistrarCampanhasComponent } from './registrar-campanhas/registrar-campanhas.component';
-import { RegistrarProductosComponent } from './registrar-productos/registrar-productos.component';
-import { ModificarClienteCampanhaComponent } from './modificar-cliente-campanha/modificar-cliente-campanha.component';
-import { AsignarCampanhasComponent } from './asignar-campanhas/asignar-campanhas.component';
-import { ModificandoClienteCampanhaComponent } from './modificando-cliente-campanha/modificando-cliente-campanha.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     NavbarComponent,
-    GenerarReporteComponent,
-    InicioComponent,
-    RegistrarCampanhasComponent,
-    RegistrarProductosComponent,
-    ModificarClienteCampanhaComponent,
-    AsignarCampanhasComponent,
-    ModificandoClienteCampanhaComponent
+    RegistrarCampanhasComponent
   ],
   imports: [
     CommonModule,
