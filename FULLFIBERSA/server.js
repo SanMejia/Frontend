840c21e547a0e5ecf067c0c42d130b33d01b6575
Santiago Mejia <santiago.mejia@jalasoft.com>
2022-02-07 21:44:06 -0500

    a

--- FULLFIBERSA/server.js
+++ FULLFIBERSA/server.js
@@ -20,7 +20,7 @@ app.get('/*', function (req, res) {
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname + '/src/app/Componentes/ingreso/ingreso.component.html'));
 }); */
-
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 8080)
+/* app.listen(process.env.PORT || 3000, function () {
     console.log("Node app is running at:" + process.env.path + app.get('port'));
-});
+}); */
