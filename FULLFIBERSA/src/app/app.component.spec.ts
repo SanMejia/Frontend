1b1aebec3c0c6f125d4d3eb87947c5a7801c7087
Santiago Mejia <santiago.mejia@jalasoft.com>
2022-02-07 22:04:11 -0500

    Change of name

--- FULLFIBERSA/src/app/app.component.spec.ts
+++ FULLFIBERSA/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FULLFIBERSA'`, () => {
+  it(`should have as title 'OVAS WEBSITE'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FULLFIBERSA');
+    expect(app.title).toEqual('OVAS WEBSITE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FULLFIBERSA app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('OVAS WEBSITE app is running!');
   });
 });
